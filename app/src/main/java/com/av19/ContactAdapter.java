cf950de4619a7c9c673874d48db0226d2a2cb4f2
AnderVicario <andervicariozabala@gmail.com>
2024-10-23 13:06:23 +0200

    List of contacts

--- app/src/main/java/com/av19/ContactAdapter.java
+++ app/src/main/java/com/av19/ContactAdapter.java
@@ -1,57 +1,57 @@
 package com.av19;
 
-import android.app.Activity;
-import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ContactAdapter extends BaseAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-    Activity myActivity;
-    ContactList contactList;
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder> {
 
-    public ContactAdapter(Activity myActivity, ContactList contactList) {
-        this.myActivity = myActivity;
+    private final ContactList contactList;
+
+    public ContactAdapter(ContactList contactList) {
         this.contactList = contactList;
     }
 
+    @NonNull
     @Override
-    public int getCount() {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.person_one_line, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Contact contact = contactList.getContacts().get(position);
+        holder.bind(contact);
+    }
+
+    @Override
+    public int getItemCount() {
         return contactList.getContacts().size();
     }
 
-    @Override
-    public Contact getItem(int i) {
-        return contactList.getContacts().get(i);
-    }
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        private final TextView tv_name;
+        private final TextView tv_last_message;
+        private final ImageView iv_icon;
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_name = itemView.findViewById(R.id.tv_name);
+            tv_last_message = itemView.findViewById(R.id.tv_last_message);
+            iv_icon = itemView.findViewById(R.id.iv_icon);
+        }
 
-    @Override
-    public View getView(int i, View view, ViewGroup parent) {
-
-        View onePersonLine;
-
-        LayoutInflater inflater = (LayoutInflater) myActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        onePersonLine = inflater.inflate(R.layout.person_one_line, parent, false);
-
-        TextView tv_name = onePersonLine.findViewById(R.id.tv_name);
-        TextView tv_last_message = onePersonLine.findViewById(R.id.tv_last_message);
-        ImageView iv_icon = onePersonLine.findViewById(R.id.iv_icon);
-
-        Contact contact = this.getItem(i);
-
-        tv_name.setText(contact.getName());
-        tv_last_message.setText(contact.getLastMessage());
-        iv_icon.setImageResource(R.drawable.icon);
-
-        return onePersonLine;
+        public void bind(Contact contact) {
+            tv_name.setText(contact.getName());
+            tv_last_message.setText(contact.getLastMessage());
+            iv_icon.setImageResource(R.drawable.icon);
+        }
     }
 }
--- app/src/main/java/com/av19/MainActivity2.java
+++ app/src/main/java/com/av19/MainActivity2.java
@@ -1,21 +1,20 @@
 package com.av19;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ListView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity2 extends AppCompatActivity {
 
     ContactList contactList;
     ContactAdapter contactAdapter;
-    ListView listView;
-    Button button_add;
+    RecyclerView recyclerView;
+    FloatingActionButton button_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +26,14 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        setUpRecyclerView();
+    }
 
+    private void setUpRecyclerView() {
         contactList = new ContactList();
-        contactAdapter = new ContactAdapter(this, contactList);
-//        button_add.findViewById(R.id.button_add);
-        listView = findViewById(R.id.listView);
-        listView.setAdapter(contactAdapter);
-
+        contactAdapter = new ContactAdapter(contactList);
+        button_add = findViewById(R.id.button_add);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(contactAdapter);
     }
 }
